4caea87f2319718a6ee4401c6796202373af6c55
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-12-26 19:40:59 +0700

    GO-3 || Implement GraphQL server initialization and mutation resolver || HauzanDev

--- graphql/graph.go
+++ graphql/graph.go
@@ -1,1 +1,40 @@
 package main
+
+// type server struct {
+// 	accountClient *account.Client
+// 	catalogClient *catalog.Client
+// 	orderClient *order.Client
+// }
+
+func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
+	accountClient, err := account.NewClient(accountUrl)
+
+	if err != nil {
+		return nil, err
+	}
+
+	cattalogClient, err := catalog.NewClient(catalogUrl)
+
+	if err != nil {
+		accountClient.Close()
+		return nil, err
+	}
+
+	orderClient, err := order.NewClient(orderUrl)
+
+	if err != nil {
+		accountClient.Close()
+		cattalogClient.Close()
+		return nil, err
+	}
+
+	return &Server{
+		accountClient,
+		cattalogClient,
+		orderClient,
+	}, nil
+}
+
+func (s *Server) Mutation() MutationResolver {
+	return &mutationResolver{server: s}
+}
