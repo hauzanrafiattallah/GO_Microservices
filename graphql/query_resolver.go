214ffbf80e8ab640045777a68019c387598df528
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-12-26 21:53:17 +0700

    GO-3 || Add GraphQL resolvers and update dependencies for account and query handling || HauzanDev

--- graphql/query_resolver.go
+++ graphql/query_resolver.go
@@ -1,5 +1,17 @@
 package main
 
+import "context"
+
 type queryResolver struct {
 	server *Server
+}
+
+func (r *queryResolver) Accounts(ctx context.Context, pagination *PaginationInput, id *string) ([]*Account, error) {
+
+	return nil, nil
+}
+
+func (r *queryResolver) Products(ctx context.Context, pagination *PaginationInput, query *string) ([]*Product, error) {
+
+	return nil, nil
 }
