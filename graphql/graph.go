e0fd6d3b82a09c7a779ed06a26b59622e090e364
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-12-27 06:05:06 +0700

    GO-4 || Implement PostgreSQL repository for account management and update GraphQL server initialization || HauzanDev

--- graphql/graph.go
+++ graphql/graph.go
@@ -1,39 +1,41 @@
 package main
 
+import "github.com/99designs/gqlgen/graphql"
+
 type Server struct {
 	// accountClient *account.Client
 	// catalogClient *catalog.Client
 	// orderClient   *order.Client
 }
 
-// func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
-// 	accountClient, err := account.NewClient(accountUrl)
+func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
+	// 	accountClient, err := account.NewClient(accountUrl)
 
-// 	if err != nil {
-// 		return nil, err
-// 	}
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
 
-// 	cattalogClient, err := catalog.NewClient(catalogUrl)
+	// 	cattalogClient, err := catalog.NewClient(catalogUrl)
 
-// 	if err != nil {
-// 		accountClient.Close()
-// 		return nil, err
-// 	}
+	// 	if err != nil {
+	// 		accountClient.Close()
+	// 		return nil, err
+	// 	}
 
-// 	orderClient, err := order.NewClient(orderUrl)
+	// 	orderClient, err := order.NewClient(orderUrl)
 
-// 	if err != nil {
-// 		accountClient.Close()
-// 		cattalogClient.Close()
-// 		return nil, err
-// 	}
+	// 	if err != nil {
+	// 		accountClient.Close()
+	// 		cattalogClient.Close()
+	// 		return nil, err
+	// 	}
 
-// 	return &Server{
-// 		accountClient,
-// 		cattalogClient,
-// 		orderClient,
-// 	}, nil
-// }
+	return &Server{
+		// accountClient,
+		// cattalogClient,
+		// orderClient,
+	}, nil
+}
 
 // func (s *Server) Mutation() MutationResolver {
 // 	return &mutationResolver{server: s}
@@ -47,8 +49,8 @@ type Server struct {
 // 	return &accountResolver{server: s}
 // }
 
-// func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
-// 	return NewExecutableSchema(Config{
-// 		Resolvers: s,
-// 	})
-// }
+func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
+	return NewExecutableSchema(Config{
+		Resolvers: s,
+	})
+}
